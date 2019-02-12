11aa4da8d2d0c820bb4fdfb14868308921b685fa
sebastien <sebastien@sebastien-ubuntu>
2019-02-12 16:13:52 +0100

    create JSP and servlet for the JSP file

--- src/JSPServlet.java
+++ src/JSPServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,9 @@ public class JSPServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher req = request.getRequestDispatcher("/HelloWorld.jsp");
+		req.forward(request, response);
 	}
 
 	/**
