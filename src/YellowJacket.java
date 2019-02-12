0775b9ecbbdade52f47a2e30cefe9c726e99ee7b
wfreau <wfreau@gmail.com>
2019-02-12 15:00:08 +0100

    Random number
    But sometime negative number need to be fix

--- src/YellowJacket.java
+++ src/YellowJacket.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 public class YellowJacket extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	enum Pays {France, Pologone, Allemagne};
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,8 +30,16 @@ public class YellowJacket extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("<html>");
+		this.printPage(response.getWriter());
 		
+		Pays p = (request.getParameter("list")==null)?null:Pays.valueOf(request.getParameter("list"));
+		
+		if(p != null)
+		{
+			this.printNbYellowJacket(p, response.getWriter());
+		}
+		response.getWriter().append("</html>");
 	}
 
 	/**
@@ -39,7 +50,21 @@ public class YellowJacket extends HttpServlet {
 	}
 	
 	private void printPage(PrintWriter print) {
+		print.append("<form ><select name=\"list\" >\n");
+		for(Pays p : Pays.values())
+		{
+			print.append("<option>").append(p.name()).append("</option>");
+		}
+		print.append("</select><input type=\"submit\" value=\"Valider!\"></form>");
+	}
+	
+	private void printNbYellowJacket(Pays p, PrintWriter print)
+	{
+		for(int i = 0 ; i < 10 ; i++)
+			print.append("<br>");
 		
+		print.append(p.name());
+		print.append(": ").append(String.valueOf((new Random()).nextInt()));
 	}
 
 }
